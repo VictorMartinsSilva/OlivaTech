3a3339bed6879c471850f8ba212509ae1f9c09df
Victor Martins <hidiku_lhiku@hotmail.com>
2021-06-12 19:52:22 -0300

    Subindo configurações do banco de dados e da startup.
    Alterando nome do banco de dados e adicionando referência do Identity User Custom e ContextDb ao Startup.

--- src/OlivaTech/OlivaTech.Site/Startup.cs
+++ src/OlivaTech/OlivaTech.Site/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OlivaTech.Site.Data;
+using OlivaTech.Site.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,11 +31,14 @@ namespace OlivaTech.Site
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<ContextDb>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+            services.AddDefaultIdentity<IdentityUserCustom>(options => options.SignIn.RequireConfirmedAccount = false)
+               .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
