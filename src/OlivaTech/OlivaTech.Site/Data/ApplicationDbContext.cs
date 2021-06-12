8757fb6ba0e524008844d60c4a1384eaf6eba741
Victor Martins <hidiku_lhiku@hotmail.com>
2021-06-12 19:45:03 -0300

    Subindo Identity User Custom.
    Subindo Identity User Custom, onde, é inserido colunas extras para compor o registro de usuário do site.

--- src/OlivaTech/OlivaTech.Site/Data/ApplicationDbContext.cs
+++ src/OlivaTech/OlivaTech.Site/Data/ApplicationDbContext.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using OlivaTech.Site.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace OlivaTech.Site.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUserCustom>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+
         }
+        
     }
 }
---
+++ src/OlivaTech/OlivaTech.Site/Models/IdentityUserCustom.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlivaTech.Site.Models
+{
+    public class IdentityUserCustom : IdentityUser
+    {
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [Display(Name = "Nome")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O {0} deve conter de {2} a {1} caracteres.")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Sobrenome")]
+        public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "A {0} é obrigatória.")]
+        [Display(Name = "Cidade")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "A {0} deve conter de {2} a {1} caracteres.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [Display(Name = "Bairro")]
+        [StringLength(120, MinimumLength = 2, ErrorMessage = "O {0} deve conter de {2} a {1} caracteres.")]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [Display(Name = "UF")]
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "O {0} deve conter de {2} a {1} caracteres.")]
+        public string UF { get; set; }
+
+        [Required(ErrorMessage = "O {0} é obrigatório.")]
+        [Display(Name = "CEP")]
+        [StringLength(9, MinimumLength = 8, ErrorMessage = "O {0} deve conter de {2} a {1} caracteres.")]
+        public string CEP { get; set; }
+    }
+}
