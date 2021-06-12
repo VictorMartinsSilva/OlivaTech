a953dc32fb98971c2d1e3df925c4e39697cf2c49
Victor Martins <hidiku_lhiku@hotmail.com>
2021-06-12 19:16:39 -0300

    Criando SupressElement.
    Criando SupressElement para gerir acessibilidade aos componentes dos site.

---
+++ src/OlivaTech/OlivaTech.Site/Extension/SuppressElementByClaimTagHelper.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OlivaTech.Site.Extension
+{
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-name")]
+    [HtmlTargetElement("*", Attributes = "supress-by-claim-value")]
+    public class SuppressElementByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public SuppressElementByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("supress-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("supress-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) 
+            { 
+                throw new ArgumentNullException(nameof(context));
+            }
+                
+            if (output == null) 
+            { 
+                throw new ArgumentNullException(nameof(output));
+            }
+                
+
+            var temAcesso = CustomAuthorization.ValidationClaimsUser(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.SuppressOutput();
+        }
+    }
+
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-name")]
+    [HtmlTargetElement("a", Attributes = "disable-by-claim-value")]
+    public class DesabilitaLinkByClaimTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public DesabilitaLinkByClaimTagHelper(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        [HtmlAttributeName("disable-by-claim-name")]
+        public string IdentityClaimName { get; set; }
+
+        [HtmlAttributeName("disable-by-claim-value")]
+        public string IdentityClaimValue { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (context == null) 
+            { 
+                throw new ArgumentNullException(nameof(context));
+            }
+                
+            if (output == null)
+            {
+                throw new ArgumentNullException(nameof(output));
+            }
+
+            var temAcesso = CustomAuthorization.ValidationClaimsUser(_contextAccessor.HttpContext, IdentityClaimName, IdentityClaimValue);
+
+            if (temAcesso) return;
+
+            output.Attributes.RemoveAll("href");
+            output.Attributes.Add(new TagHelperAttribute("style", "cursor: not-allowed"));
+            output.Attributes.Add(new TagHelperAttribute("title", "Você não tem permissão"));
+        }
+    }
+}
+
