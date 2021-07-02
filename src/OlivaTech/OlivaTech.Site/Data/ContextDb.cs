d31ada17a08d9392af25491310892b582ac9c702
Victor Martins <hidiku_lhiku@hotmail.com>
2021-07-01 21:17:30 -0300

    Criando Database Seeding.
    Criando Database Seeding das tabelas, curso, tipo de curso e oferta.

--- src/OlivaTech/OlivaTech.Site/Data/ContextDb.cs
+++ src/OlivaTech/OlivaTech.Site/Data/ContextDb.cs
@@ -14,9 +14,31 @@ namespace OlivaTech.Site.Data
         {
 
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CursoTipo>().HasData(
+                new CursoTipo { CursoTipoId = 1, Nome = "Pós-Graduação" },
+                new CursoTipo { CursoTipoId = 2, Nome = "Bacharelado"},
+                new CursoTipo { CursoTipoId = 3, Nome = "Técnico" },
+                new CursoTipo { CursoTipoId = 4, Nome = "Mestrado"});
+
+            modelBuilder.Entity<Curso>().HasData(
+                new Curso { CursoTipoId = 2, CursoId = 1, Nome = "Sistema de Informação", Cidade = "Volta Redonda", UF = "RJ", Disponivel = true },
+                new Curso { CursoTipoId = 1, CursoId = 2, Nome = "Tecnologias para Aplicações Web", Cidade = "Serrana", UF = "SP", Disponivel = true },
+                new Curso { CursoTipoId = 3, CursoId = 3, Nome = "Informática", Cidade = "Volta Redonda", UF = "RJ", Disponivel = false });
+
+            modelBuilder.Entity<Oferta>().HasData(
+                
+                new Oferta { CursoId = 1, OfertaId = 1, Preco = Convert.ToDecimal(306.76) , Disponivel = true },
+                new Oferta { CursoId = 2, OfertaId = 2, Preco = Convert.ToDecimal(158.58) , Disponivel = true },
+                new Oferta { CursoId = 3, OfertaId = 3, Preco = Convert.ToDecimal(230.50) , Disponivel = false });
+
+            
+        }
 
         public DbSet<Curso> Cursos { get; set; }
         public DbSet<CursoTipo> CursoTipos { get; set; }
+        public DbSet<Oferta> Ofertas { get; set; }
        
 
 
---
+++ src/OlivaTech/OlivaTech.Site/Migrations/ContextDbMigrations/20210701071735_AddInitial.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OlivaTech.Site.Migrations.ContextDbMigrations
+{
+    public partial class AddInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CursoTipos",
+                columns: table => new
+                {
+                    CursoTipoId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CursoTipos", x => x.CursoTipoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cursos",
+                columns: table => new
+                {
+                    CursoId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(42)", maxLength: 42, nullable: false),
+                    Cidade = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UF = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    Disponivel = table.Column<bool>(type: "bit", nullable: false),
+                    CursoTipoId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cursos", x => x.CursoId);
+                    table.ForeignKey(
+                        name: "FK_Cursos_CursoTipos_CursoTipoId",
+                        column: x => x.CursoTipoId,
+                        principalTable: "CursoTipos",
+                        principalColumn: "CursoTipoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ofertas",
+                columns: table => new
+                {
+                    OfertaId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Preco = table.Column<decimal>(type: "decimal(12,2)", nullable: false),
+                    Disponivel = table.Column<bool>(type: "bit", nullable: false),
+                    CursoId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ofertas", x => x.OfertaId);
+                    table.ForeignKey(
+                        name: "FK_Ofertas_Cursos_CursoId",
+                        column: x => x.CursoId,
+                        principalTable: "Cursos",
+                        principalColumn: "CursoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CursoTipos",
+                columns: new[] { "CursoTipoId", "Nome" },
+                values: new object[,]
+                {
+                    { 1L, "Pós-Graduação" },
+                    { 2L, "Bacharelado" },
+                    { 3L, "Técnico" },
+                    { 4L, "Mestrado" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Cursos",
+                columns: new[] { "CursoId", "Cidade", "CursoTipoId", "Disponivel", "Nome", "UF" },
+                values: new object[] { 2L, "Serrana", 1L, true, "Tecnologias para Aplicações Web", "SP" });
+
+            migrationBuilder.InsertData(
+                table: "Cursos",
+                columns: new[] { "CursoId", "Cidade", "CursoTipoId", "Disponivel", "Nome", "UF" },
+                values: new object[] { 1L, "Volta Redonda", 2L, true, "Sistema de Informação", "RJ" });
+
+            migrationBuilder.InsertData(
+                table: "Cursos",
+                columns: new[] { "CursoId", "Cidade", "CursoTipoId", "Disponivel", "Nome", "UF" },
+                values: new object[] { 3L, "Volta Redonda", 3L, false, "Informática", "RJ" });
+
+            migrationBuilder.InsertData(
+                table: "Ofertas",
+                columns: new[] { "OfertaId", "CursoId", "Disponivel", "Preco" },
+                values: new object[] { 2L, 2L, true, 158.58m });
+
+            migrationBuilder.InsertData(
+                table: "Ofertas",
+                columns: new[] { "OfertaId", "CursoId", "Disponivel", "Preco" },
+                values: new object[] { 1L, 1L, true, 306.76m });
+
+            migrationBuilder.InsertData(
+                table: "Ofertas",
+                columns: new[] { "OfertaId", "CursoId", "Disponivel", "Preco" },
+                values: new object[] { 3L, 3L, false, 230.5m });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cursos_CursoTipoId",
+                table: "Cursos",
+                column: "CursoTipoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ofertas_CursoId",
+                table: "Ofertas",
+                column: "CursoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ofertas");
+
+            migrationBuilder.DropTable(
+                name: "Cursos");
+
+            migrationBuilder.DropTable(
+                name: "CursoTipos");
+        }
+    }
+}
